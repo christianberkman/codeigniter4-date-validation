26effe0a67271f591810e30cf4bc0aebbf95bde4
Christian Berkman <christianberkman@gmail.com>
2024-06-27 22:34:12 +0300

    add beginning_today, beginning_tomorrow

--- src/DateValidation.php
+++ src/DateValidation.php
@@ -1,9 +1,102 @@
 <?php
 namespace ChristianBerkman\DateValidation;
 
-class DateValidation{
+use \DateTime;
+use \DateTimeZone;
 
-	public function datetest($str, $format, $data){
+class DateValidation{
+	/**
+	 * Public static strings
+	 */
+	public static string $invalidDate = 'Invalid date.';
+
+	/**
+     * Checks for a valid date using a given format and returns a DateTime object, ignoring time information
+	 * Based on CodeIgniter4/framework/sytem/Validation/FormatRules::valid_date
+     *
+     * @param string|null           $str
+     * @param non-empty-string|null $format
+	 * @return DateTime|bool
+     */
+    public static function createDate(string $value, ?string $format = null): DateTime|bool
+    {
+        if ($value === '') {
+            return false;
+        }
+		
+        if ($format === null || $format === '') {
+			if (! strtotime($value)) return false;
+			$date = new DateTime($value);
+        } else {
+			$date = DateTime::createFromFormat($format, $value);
+		}
+		
+        if ($date === false) {
+			return false;
+        }
+		
+		$errors = DateTime::getLastErrors();
+		if ($errors['warning_count'] !== 0 || $errors['error_count'] !== 0) return false;
+		
+		return $date->setTimeZone(new DateTimezone('UTC'))->setTime(0,0);
+    }
+
+	/**
+	 * Return a DateTime object of today's date
+	 *
+	 * @return DateTime
+	 */
+	public static function today(): DateTime
+	{
+		return (new DateTime('today'))->setTimeZone(new DateTimezone('UTC'))->setTime(0,0);
+	}
+	
+	/**
+	 * Validate if the value date is today or any date after today
+	 * Examples:
+	 * 		beginning_today[Y-m-d]
+	 * 		beginning_today[]       value parsed by strtotime()
+	 *
+	 * @param string $value
+	 * @param string $format
+	 * @param array $data
+	 * @param string|null $error
+	 * @return bool
+	 */
+	public function beginning_today(string $value, string $format, array $data, ?string &$error): bool
+	{
+		// Check value date
+		if (! $valueDate = static::createDate($value, $format))
+		{
+			$error = static::$invalidDate;
+			return false;
+		}
+
+		// Compare value date to today
+		if ($valueDate->getTimestamp() < static::today()->getTimestamp())
+		{
+			$error = "Date must be today or later.";
+			return false;
+		}
+
+		return true;
+	}
+
+	public function beginning_tomorrow(string $value, string $format, array $data, ?string &$error): bool
+	{
+		// Check value date
+		if (! $valueDate = static::createDate($value, $format))
+		{
+			$error = static::$invalidDate;
+			return false;
+		}
+
+		if ($valueDate->getTimestamp() <= static::today()->getTimestamp())
+		{
+			$error = "Date must be tomorrow or later.";
+			return false;
+		}
+
 		return true;
 	}
 }
